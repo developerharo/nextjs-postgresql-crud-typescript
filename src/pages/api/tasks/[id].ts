40ccccaef1205e173c7d8c406133502f369da4a4
developerharo <developerharo@gmail.com>
2021-12-04 17:49:46 -0600

    Unique task endpoint done

--- src/pages/api/tasks/[id].ts
+++ src/pages/api/tasks/[id].ts
@@ -1,31 +1,53 @@
-import { NextApiRequest, NextApiResponse } from 'next'
+import { NextApiRequest, NextApiResponse } from 'next';
+import { conn } from '../../../utils/database';
 
 // eslint-disable-next-line import/no-anonymous-default-export
-export default (req: NextApiRequest, res: NextApiResponse) => {
-  
-  const { method } = req;
+export default async (req: NextApiRequest, res: NextApiResponse) => {  
+  const { method, query, body } = req;
 
   switch (method) {
     case 'GET':
-      return res.status(200).json(
-        {      
-          "task": "getting a unique task"    
-        }
-      )    
+      try {
+        const text = "SELECT * FROM tasks WHERE id = $1";
+        const values = [query.id];
+        const result = await conn.query(text, values);
+        
+        if (result.rows.length === 0)
+          return res.status(404).json({message: "Task not found"});
+          
+        return res.status(200).json(result.rows[0]);    
+      } catch (error: any) {
+        return res.status(500).json({ message: error.message });
+      }      
       break;
     case 'PUT':
-      return res.status(200).json(
-        {      
-          "task": "updating a unique task"    
-        }
-      )    
+      try {
+        const { title, description } = body;
+        const text = "UPDATE tasks SET title = $1, description = $2 WHERE id = $3 RETURNING *";
+        const values = [title, description, query.id];
+        const result = await conn.query(text, values);
+        
+        if (result.rows.length === 0)
+          return res.status(404).json({message: "Task not found"});
+          
+        return res.status(200).json(result.rows[0]);    
+      } catch (error: any) {
+        return res.status(500).json({ message: error.message });
+      }    
       break;
     case 'DELETE':
-      return res.status(200).json(
-        {      
-          "task": "deleting a unique task"    
-        }
-      )    
+      try {
+        const text = "DELETE FROM tasks WHERE id = $1 RETURNING *";
+        const values = [query.id];
+        const result = await conn.query(text, values);
+                
+        if (result.rowCount === 0)
+          return res.status(404).json({message: "Task not found"});
+          
+        return res.status(200).json(result.rows[0]);    
+      } catch (error: any) {
+        return res.status(500).json({ message: error.message });
+      }
       break;
     default:
       return res.status(400).json({
